7a3b9a0f0e26898db827a967f568bbc9b1941856
DrHackenstein <hiddenlogin@mailbox.org>
2024-07-28 23:19:26 +0200

    Added basic scene auto save system

--- scripts/content_manager.gd
+++ scripts/content_manager.gd
@@ -3,11 +3,24 @@ class_name Content_Manager extends Node
 @export var mod_window : Window
 @export var chat_window : Window
 
-var scenario = ""
+var saves : ConfigFile
 var content = {}
 
+var start = "1"
+var scenarios = []
+var scenario = ""
+
 func _ready():
-	
+	load_save()
+	load_content_file()
+
+	#Load Start Message
+	self.load_content(content["G1"].id, true)
+	#self.load_content(content["M2b"].id, true)
+	#self.load_content(content["C1"].id, true)
+	#self.load_content(content["E1"].id, true)
+
+func load_content_file():
 	var count = 0
 	var file = FileAccess.open("res://content/content.csv.txt", FileAccess.READ)
 	var data
@@ -33,22 +46,20 @@ func _ready():
 		line.text = data[1]
 		line.uid = data[2]
 		line.wid = data[3]
-		line.follow = data[4].split(",")
-		line.buttons = data[5].split(",")
+		if data[4] != "":
+			line.follow = data[4].split(",")
+		if data[5] != "":
+			line.buttons = data[5].split(",")
 		line.okay = data[6] != "0"
 		line.sid = data[7]
 		line.trigger = data[8]
+		if line.trigger == "start":
+			scenarios.append(line.id)
 		
 		#Add line to content library
 		content[ line.id ] = line
-		#print(line.text)
-			
+		
 	file.close()
-	
-	#Load Start Message
-	self.load_content(content["1"].id, true)
-	#self.load_content(content["M2b"].id, true)
-	#self.load_content(content["E1"].id, true)
 
 func load_content( id : String, follow : bool, parent : String = "" ):
 	
@@ -59,7 +70,13 @@ func load_content( id : String, follow : bool, parent : String = "" ):
 	
 	print("Loading ", id)
 	
-	var content = self.content[id]
+	var content : Content = self.content[id]
+	
+	if content.trigger == "start":
+		save_start(content.id)
+	
+	if content.trigger == "end" && content.follow.size() < 1:
+		reset_saves()
 	
 	if parent != "":
 		content.parent = parent
@@ -80,5 +97,23 @@ func load_content( id : String, follow : bool, parent : String = "" ):
 		print("Loading follows of ", id, ":")
 		for i in content.follow:
 			load_content( i, false, id )
+
+func load_save():
+	saves = ConfigFile.new()
+	var load = saves.load("user://saves.cfg")
 	
-	
+	if load == OK:
+		start = saves.get_value("main", "start", "1")
+
+func reset_saves():
+	print("Reset saves")
+	saves.clear()
+	saves.save("user://saves.cfg")
+
+func save_start(id : String):
+	if(scenarios.find(id) <= scenarios.find(start)):
+		return
+	print("Save: ", id)
+	start = id
+	saves.set_value("main", "start", id)
+	saves.save("user://saves.cfg")
